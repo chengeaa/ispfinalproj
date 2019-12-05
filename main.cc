31c01e40061ef387d2fac87c28f404b0c3e37b80
Erik Cheng <ckaiwen2@hotmail.com>
2019-12-04 23:24:01 -0600

    greedy route works I think

--- main.cc
+++ main.cc
@@ -10,12 +10,14 @@ using std::vector;
 using std::shared_ptr;
 using std::make_shared;
 using std::cout;
+using std::cerr;
 using std::cin;
 using std::endl;
 
 class Address {
     private:
         float i, j;
+        int last_date;
     public:
         Address(float i, float j): i(i), j(j) {};
         string as_string(){
@@ -28,17 +30,17 @@ class Address {
             return output;
         }
         void print(){
-            cout << "(" << i << ", " << j << ")" << endl;
+            cout << "(" << i << ", " << j << ") ";
         }
-        float l1(Address other){
-            return abs(i - other.i) + abs(j - other.j);
-        }
-        float l2(Address other){
+        float distance(Address other){
+            // chose to implement distance using L2 norm to match example output
             float di = i - other.i;
             float dj = j - other.j;
             return sqrt(di*di + dj*dj);
         }
-
+        int get_last_date(){
+            return last_date;
+        }
 };
 
 class AddressList {
@@ -48,9 +50,20 @@ class AddressList {
         AddressList(){
             addresses = {};
         }
-        void add_address(Address a){
-            addresses.push_back(a);
-
+        AddressList(vector<Address> source){
+            addresses = source;
+        }
+        void add_address(Address newaddress){
+            // adds address a to vector 'addresses' if and only if 
+            // l1 norm of a to all addresses in addresses is not 0
+            for (auto a: addresses) {
+                if (newaddress.distance(a)  == 0.0){ 
+                    cout << "Address not added; identical to current member" << endl;
+                    cout << "For " << a.as_string() << "," << newaddress.as_string() << endl;
+                    return;
+                } 
+            }
+            addresses.push_back(newaddress);
         }
         int size(){
             return addresses.size();
@@ -59,56 +72,56 @@ class AddressList {
             for (Address a: addresses){
                 a.print();
             }
+            cout << endl;
         }
-        float length(string norm){
-            // returns total length of path if added addresses  traversed in order, based on specified norm
+        float length(){
+            // returns total length of path if added addresses traversed in order
             float total = 0;
             Address last = addresses[0];
             for (int i = 1; i < addresses.size(); i++){
                 Address curr = addresses[i];
-                if (norm == "l1"){
-                    total += last.l1(curr);
-                } else {
-                    total += last.l2(curr);
-                }
+                total += last.distance(curr);
                 last = curr;
             }
             return total;
         }
-        int index_closest_to(Address a, string norm){
+        int index_closest_to(Address a){
             //Assume a is not represented in the list
-            //(ie), no 0 distances allowed
+            //ie, no 0 distances allowed
             int closest = 0;
-            float mindist = std::numeric_limits<float>::max();
+            float min_dist = std::numeric_limits<float>::max();
             for (int i = 0; i < addresses.size(); i++){
                 Address curr = addresses[i];
-                float thisdist;
-                //calculate target node to current node distance 
-                //based on specified norm
-                if(norm == "l1"){
-                    thisdist = a.l1(curr);
-                } else {
-                    thisdist = a.l2(curr);
-                }
+                float this_dist;
+                this_dist = a.distance(curr);
                 //set new min if this is closer
-                if(thisdist < mindist){
+                if(this_dist < min_dist){
                     closest = i;
-                    mindist = thisdist;
+                    min_dist = this_dist;
                 }
             }
+            cout << "closest element to ";
+            a.print();
+            cout << "in ";
+            print();
+            cout << "is ";
+            addresses[closest].print();
+            cout << endl;
             return closest;
         }
         
-        vector<Address> greedy_route(string norm){
-            vector<Address> stops = addresses;
+        vector<Address> greedy_route(){
             vector<Address> result; 
-            Address we_are_here = Address(0,0);
+            Address we_are_here = addresses[0]; // assume depot is first item in address list;
+            // terrible idea abstraction-wise, but the spec says to, so...
             result.push_back(we_are_here);
-            //iterate through 'stops', append and erase as we go
-            while(stops.size() > 0){
-                int closest_address_index = index_closest_to(we_are_here, norm);
-                result.push_back(stops[closest_address_index]);
-                stops.erase(stops.begin() + closest_address_index);
+            addresses.erase(addresses.begin());
+            //iterate through 'addresses', append and erase as we go
+            while(addresses.size() > 0){
+                int closest_address_index = index_closest_to(we_are_here);
+                we_are_here = addresses[closest_address_index];
+                result.push_back(we_are_here);
+                addresses.erase(addresses.begin() + closest_address_index);
             }
             return result; 
         }
@@ -120,23 +133,32 @@ class Route : public AddressList {
         Address depot = Address(0,0);
     public:
         Route() : AddressList(){
-            AddressList::add_address(depot);
-            AddressList::add_address(depot);
+            addresses = {
+                depot, depot
+            };
         }
+        Route(vector<Address> source) : AddressList(source){}
 
         void add_address(Address newaddress){
-            for (auto a: addresses) {
-                if (newaddress.l1(a)  == 0.0){ //choice of metric doesn't matter here
-                    cout << "Address not added; identical to current member" << endl;
-                    cout << "For " << a.as_string() << "," << newaddress.as_string() << endl;
-                    return;
-                } 
-            }
             addresses.pop_back();
-            AddressList::add_address(newaddress);
-            AddressList::add_address(depot);
+            AddressList :: add_address(newaddress);
+            addresses.push_back(depot);
         }
-        Route greedy_route(string norm){
+
+        Route greedy_route(){
+            addresses.pop_back();
+            AddressList subvector = AddressList(addresses);
+            cout << "subvector: ";
+            subvector.print();
+            cout << "greedy output: " << endl;
+            vector<Address> greedy_output = subvector.greedy_route();
+            // construct greedy route output
+            greedy_output.push_back(depot);
+            return Route(greedy_output);
+        }
+
+        /*
+        Route greedy_route(){
             vector<Address> stops = addresses;
             Route result;
             
@@ -154,7 +176,7 @@ class Route : public AddressList {
             cout << "pruning step:" <<endl;
             //iterate through 'stops', append and erase as we go
             while(stops.size() > 0){
-                int closest_address_index = index_closest_to(we_are_here, norm);
+                int closest_address_index = index_closest_to(we_are_here);
                 we_are_here = stops[closest_address_index]; 
                 we_are_here.print() ;
                 result.add_address(we_are_here);
@@ -163,6 +185,7 @@ class Route : public AddressList {
             }
             return result; 
         }
+        */
 
         string as_string(){
             string output = "";
@@ -175,52 +198,36 @@ class Route : public AddressList {
         
 };
 
-void testfunc(){
+void closest_index_test(){
+    AddressList deliveries;
+    deliveries.add_address( Address(5,5) );
+    deliveries.add_address( Address(0,5) );
+    deliveries.print();
+    cout << deliveries.index_closest_to( Address(5,0) ) << endl;
+}
+void exampletest(){
     Route deliveries;
     deliveries.add_address( Address(0,5) );
     deliveries.add_address( Address(5,0) );
     deliveries.add_address( Address(5,5) );
-    cout << "Travel in order: " << deliveries.length("l1") << "\n";
+    cerr << "Travel in order: " << deliveries.length() << "\n";
     cout << deliveries.as_string() << endl;
     assert( deliveries.size()==5 );
     cout <<endl;
     cout << "now for the greedy route:" << endl << endl;
-    Route route = deliveries.greedy_route("l1");
+    auto route = deliveries.greedy_route();
     assert( route.size()==5 );
-    float len = route.length("l1");
-    cout << "Square route: " << route.as_string()
+    auto len = route.length();
+    cerr << "Square route: " << route.as_string()
     << "\n has length " << len << "\n";
 }
 
+void distance_test(){
+    Address one(1., 1.), two(2., 2.);
+    cerr << "Distance: " << one.distance(two) << "\n";
+}
 int main() {
-        /*
-        Address a1 = Address(1, 1);
-        Address a2 = Address(2, 2);
-        Address a3 = Address(3, 3);
-        */
-        /*
-        cout << "L1: " << a1.l1(a2) << endl;
-        cout << "L2: " << a1.l2(a2) << endl;
-        AddressList al = AddressList();
-        al.add_address(a1);
-        al.add_address(a2);
-        al.add_address(a3);
-        cout << "L1 length:  " << al.length("l1") << endl;
-        cout << "L2 length:  " << al.length("l2") << endl;
-        al.print();
-
-        Address a4 = Address(2, 2.2);
-        cout << al.index_closest_to(a4, "l1") << endl;
-        cout << al.index_closest_to(a4, "l2") << endl;
-        */
-        /*
-        Route r = Route();
-        r.add_address(a1);
-        r.add_address(a2);
-        r.add_address(a3);
-        r.print();
-        */
-    testfunc();
+    exampletest();
     return 0;
 }
 
